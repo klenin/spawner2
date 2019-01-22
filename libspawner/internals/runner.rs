1638e9ad145a934f735b0b24c0e605b6350ad728
ptslog <hsfk@yandex.ru>
2019-01-23 00:18:15 +1000

    runner: move implementation into internals

---
+++ libspawner/internals/runner.rs
@@ -0,0 +1,115 @@
+use command::Command;
+use process::{Process, Statistics, Status, Stdio};
+use runner::{ExitStatus, Report, Runner, TerminationReason};
+use std::io;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::thread::{self, JoinHandle};
+
+pub struct WaitHandle {
+    monitoring_thread: JoinHandle<io::Result<Report>>,
+    runner: Runner,
+}
+
+struct MonitoringLoop {
+    cmd: Command,
+    stats: Statistics,
+    exit_status: Option<ExitStatus>,
+    is_killed: Arc<AtomicBool>,
+}
+
+pub fn run(cmd: Command, stdio: Stdio) -> io::Result<WaitHandle> {
+    let monitoring_loop = MonitoringLoop::new(cmd);
+    let is_killed = Arc::downgrade(&monitoring_loop.is_killed);
+
+    thread::Builder::new()
+        .spawn(move || MonitoringLoop::start(monitoring_loop, stdio))
+        .map(|handle| WaitHandle {
+            monitoring_thread: handle,
+            runner: Runner {
+                is_killed: is_killed,
+            },
+        })
+}
+
+impl WaitHandle {
+    pub fn runner(&self) -> &Runner {
+        &self.runner
+    }
+
+    pub fn wait(self) -> io::Result<Report> {
+        match self.monitoring_thread.join() {
+            Ok(result) => result,
+            Err(_) => Err(io::Error::new(
+                io::ErrorKind::Other,
+                "monitoring thread panicked",
+            )),
+        }
+    }
+}
+
+impl MonitoringLoop {
+    fn new(cmd: Command) -> Self {
+        Self {
+            cmd: cmd,
+            stats: Statistics::zeroed(),
+            exit_status: None,
+            is_killed: Arc::new(AtomicBool::new(false)),
+        }
+    }
+
+    fn is_limits_exceeded(&mut self) -> bool {
+        let limits = &self.cmd.limits;
+        let stats = &self.stats;
+        let term_reason = if stats.total_processes > limits.max_processes {
+            TerminationReason::Other
+        } else if stats.total_user_time > limits.max_user_time {
+            TerminationReason::UserTimeLimitExceeded
+        } else if stats.total_bytes_written > limits.max_output_size {
+            TerminationReason::WriteLimitExceeded
+        } else if stats.peak_memory_used > limits.max_memory_usage {
+            TerminationReason::MemoryLimitExceeded
+        } else {
+            return false;
+        };
+
+        self.exit_status = Some(ExitStatus::Terminated(term_reason));
+        true
+    }
+
+    fn should_terminate(&mut self, process: &Process) -> io::Result<bool> {
+        match process.status()? {
+            Status::Alive(stats) => {
+                self.stats = stats;
+                Ok(self.is_limits_exceeded())
+            }
+            Status::Finished(code) => {
+                self.exit_status = Some(ExitStatus::Normal(code));
+                Ok(true)
+            }
+        }
+    }
+
+    fn start(mut self, stdio: Stdio) -> io::Result<Report> {
+        let process = Process::spawn(&self.cmd, stdio)?;
+        while !self.is_killed.load(Ordering::SeqCst) {
+            match self.should_terminate(&process) {
+                Ok(terminate) => {
+                    if terminate {
+                        break;
+                    }
+                }
+                Err(e) => return Err(e),
+            }
+            thread::sleep(self.cmd.monitor_interval);
+        }
+
+        Ok(Report {
+            command: self.cmd,
+            statistics: self.stats,
+            exit_status: self
+                .exit_status
+                .unwrap_or(ExitStatus::Terminated(TerminationReason::Other)),
+        })
+    }
+}
--- libspawner/runner.rs
+++ libspawner/runner.rs
@@ -1,15 +1,10 @@
 use command::Command;
-use std::io;
+use process::Statistics;
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::{Arc, Weak};
-use std::thread::{self, JoinHandle};
-use std::time::Duration;
-use std::u64;
-use sys::process::{ProcessTree, ProcessTreeStatus, SummaryInfo};
+use std::sync::Weak;
 
-#[derive(Copy, Clone)]
+#[derive(Clone)]
 pub enum TerminationReason {
-    None,
     UserTimeLimitExceeded,
     WriteLimitExceeded,
     MemoryLimitExceeded,
@@ -17,43 +12,21 @@ pub enum TerminationReason {
 }
 
 #[derive(Clone)]
+pub enum ExitStatus {
+    Normal(i32),
+    Terminated(TerminationReason),
+}
+
+#[derive(Clone)]
 pub struct Report {
-    pub cmd: Command,
-    pub termination_reason: TerminationReason,
-    pub user_time: Duration,
-    pub kernel_time: Duration,
-    pub peak_memory_used: u64,
-    pub processes_created: u64,
-    pub exit_code: i32,
+    pub command: Command,
+    pub statistics: Statistics,
+    pub exit_status: ExitStatus,
 }
 
 #[derive(Clone)]
 pub struct Runner {
-    is_killed: Weak<AtomicBool>,
-}
-
-pub(crate) struct WaitHandle {
-    monitoring_thread: JoinHandle<io::Result<Report>>,
-    runner: Runner,
-}
-
-struct MonitoringLoop {
-    cmd: Command,
-    is_killed: Arc<AtomicBool>,
-}
-
-pub(crate) fn run(cmd: Command) -> io::Result<WaitHandle> {
-    let monitoring_loop = MonitoringLoop::new(cmd);
-    let is_killed = Arc::downgrade(&monitoring_loop.is_killed);
-
-    thread::Builder::new()
-        .spawn(move || MonitoringLoop::start(monitoring_loop))
-        .map(|handle| WaitHandle {
-            monitoring_thread: handle,
-            runner: Runner {
-                is_killed: is_killed,
-            },
-        })
+    pub(crate) is_killed: Weak<AtomicBool>,
 }
 
 impl Runner {
@@ -61,87 +34,5 @@ impl Runner {
         if let Some(flag) = self.is_killed.upgrade() {
             flag.store(true, Ordering::SeqCst);
         }
-    }
-}
-
-impl WaitHandle {
-    pub(crate) fn runner(&self) -> &Runner {
-        &self.runner
-    }
-
-    pub(crate) fn wait(self) -> io::Result<Report> {
-        match self.monitoring_thread.join() {
-            Ok(result) => result,
-            Err(_) => Err(io::Error::new(
-                io::ErrorKind::Other,
-                "monitoring thread panicked",
-            )),
-        }
-    }
-}
-
-impl MonitoringLoop {
-    fn new(cmd: Command) -> Self {
-        Self {
-            cmd: cmd,
-            is_killed: Arc::new(AtomicBool::new(false)),
-        }
-    }
-
-    fn termination_reason(&self, info: &SummaryInfo) -> TerminationReason {
-        let limits = &self.cmd.limits;
-        if info.total_processes > limits.max_processes {
-            TerminationReason::Other
-        } else if info.total_user_time > limits.max_user_time {
-            TerminationReason::UserTimeLimitExceeded
-        } else if info.total_bytes_written > limits.max_output_size {
-            TerminationReason::WriteLimitExceeded
-        } else if info.peak_memory_used > limits.max_memory_usage {
-            TerminationReason::MemoryLimitExceeded
-        } else {
-            TerminationReason::None
-        }
-    }
-
-    fn start(self) -> io::Result<Report> {
-        let mut summary_info = SummaryInfo::zeroed();
-        let mut termination_reason = TerminationReason::None;
-        let mut exit_code = 0;
-        let pstree = ProcessTree::spawn(&self.cmd.inner)?;
-
-        while !self.is_killed.load(Ordering::SeqCst) {
-            match pstree.status() {
-                Ok(status) => match status {
-                    ProcessTreeStatus::Alive(info) => {
-                        summary_info = info;
-                        termination_reason = self.termination_reason(&summary_info);
-                        match termination_reason {
-                            TerminationReason::None => {}
-                            _ => break,
-                        }
-                    }
-                    ProcessTreeStatus::Finished(c) => {
-                        exit_code = c;
-                        break;
-                    }
-                },
-                Err(e) => {
-                    pstree.kill()?;
-                    return Err(e);
-                }
-            }
-            thread::sleep(self.cmd.monitor_interval);
-        }
-
-        pstree.kill()?;
-        Ok(Report {
-            cmd: self.cmd,
-            termination_reason: termination_reason,
-            user_time: summary_info.total_user_time,
-            kernel_time: summary_info.total_kernel_time,
-            peak_memory_used: summary_info.peak_memory_used,
-            processes_created: summary_info.total_processes,
-            exit_code: exit_code,
-        })
     }
 }
