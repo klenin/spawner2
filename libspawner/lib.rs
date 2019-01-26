145f942a71f4c7836d44860ff64944eefd397fe9
ptslog <hsfk@yandex.ru>
2019-01-26 19:00:22 +1000

    libspawner: Update lib.rs

--- libspawner/lib.rs
+++ libspawner/lib.rs
@@ -1,10 +1,13 @@
 extern crate cfg_if;
+extern crate spawner_opts;
 
 #[cfg(windows)]
 extern crate winapi;
 
 pub mod command;
+pub mod driver;
 pub mod runner;
+pub mod session;
 
 pub mod process {
     pub use sys::process::*;
@@ -14,8 +17,6 @@ pub mod pipe {
     pub use sys::pipe::*;
 }
 
-pub use self::spawner::*;
-
-mod internals;
-mod spawner;
+mod io;
+mod runner_private;
 mod sys;
