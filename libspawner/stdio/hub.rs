269181cdf3c8d4c2f4a8b12e0cba4fd52c099c94
ptslog <hsfk@yandex.ru>
2019-02-04 18:31:03 +1000

    WriteHub: Remove buffer

--- libspawner/stdio/hub.rs
+++ libspawner/stdio/hub.rs
@@ -1,10 +1,9 @@
 use crate::{Error, Result};
 use pipe::{ReadPipe, WritePipe};
 use std::io::{Read, Write};
-use std::sync::mpsc::{channel, Receiver, RecvTimeoutError, Sender};
+use std::sync::mpsc::{channel, Receiver, Sender};
 use std::sync::Arc;
 use std::thread::{self, JoinHandle};
-use std::time::Duration;
 
 /// This structure splits the `ReadPipe` allowing multiple readers to receive data from it.
 pub struct ReadHub {
@@ -16,8 +15,6 @@ pub struct ReadHub {
 struct WriteHubInner {
     dst: WritePipe,
     receiver: Receiver<Message>,
-    buffer: Vec<u8>,
-    buffer_size: usize,
 }
 
 /// This structure splits the `WritePipe` allowing multiple writers to send data to it.
@@ -94,8 +91,6 @@ impl WriteHub {
             inner: WriteHubInner {
                 dst: dst,
                 receiver: r,
-                buffer: Vec::new(),
-                buffer_size: 4096,
             },
             sender: s,
         }
@@ -117,46 +112,13 @@ impl WriteHub {
 }
 
 impl WriteHubInner {
-    fn bufferize(&mut self, msg: Message) {
-        self.buffer.extend_from_slice(msg.get());
-    }
-
-    fn try_fill_buffer(&mut self) {
-        for _ in 0..50 {
-            match self.receiver.recv_timeout(Duration::from_millis(1)) {
-                Ok(msg) => {
-                    self.bufferize(msg);
-                    if self.buffer.len() > self.buffer_size {
-                        break;
-                    }
-                }
-                Err(e) => {
-                    if e == RecvTimeoutError::Disconnected {
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
     fn main_loop(mut self) {
-        self.buffer = Vec::with_capacity(self.buffer_size);
         loop {
-            self.buffer.clear();
             let msg = match self.receiver.recv() {
                 Ok(x) => x,
                 Err(_) => return,
             };
-
-            let data = if msg.get().len() == self.buffer_size {
-                msg.get()
-            } else {
-                self.bufferize(msg);
-                self.try_fill_buffer();
-                &self.buffer
-            };
-
-            if self.dst.write_all(data).is_err() || self.dst.flush().is_err() {
+            if self.dst.write_all(msg.get()).is_err() {
                 return;
             }
         }
