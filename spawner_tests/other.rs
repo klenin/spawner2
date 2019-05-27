32d0703ea6ec6ba49dffc05e6243b7b1d496141a
ptslog <hsfk@yandex.ru>
2019-05-27 14:46:23 +1000

    Add tests on exclusive read\write

--- spawner_tests/other.rs
+++ spawner_tests/other.rs
@@ -16,12 +16,10 @@ fn exclusive_read() {
     write_all(&file, "data");
     run(&[
         "--separator=@",
-        "-d=1",
+        "-d=2",
         "--@",
         format!("--in=*e:{}", file).as_str(),
         APP,
-        "loop",
-        "1",
         "--@",
         format!("--out={}", stdout).as_str(),
         APP,
@@ -42,12 +40,10 @@ fn exclusive_write() {
 
     run(&[
         "--separator=@",
-        "-d=1",
+        "-d=2",
         "--@",
         format!("--out=*e:{}", file).as_str(),
         APP,
-        "loop",
-        "1",
         "--@",
         format!("--out={}", stdout).as_str(),
         APP,
@@ -56,6 +52,70 @@ fn exclusive_write() {
     ])
     .unwrap();
 
+    assert_eq!("err", read_all(stdout));
+}
+
+#[cfg(windows)]
+#[test]
+fn exclusive_write_2() {
+    let tmp = TmpDir::new();
+    let file = tmp.file("file.txt");
+    let stdout = tmp.file("stdout.txt");
+
+    run(&[
+        "--separator=@",
+        "-d=1",
+        "--@",
+        format!("--out=*e:{}", file).as_str(),
+        APP,
+        "A",
+        "--@",
+        format!("--out=*e:{}", file).as_str(),
+        APP,
+        "A",
+        "--@",
+        format!("--out={}", stdout).as_str(),
+        APP,
+        "try_open",
+        file.as_str(),
+    ])
+    .unwrap();
+
+    assert_eq!("AA", read_all(file));
+    assert_eq!("err", read_all(stdout));
+}
+
+#[cfg(windows)]
+#[test]
+fn exclusive_read_2() {
+    let tmp = TmpDir::new();
+    let stdin = tmp.file("stdin.txt");
+    let stderr = tmp.file("stderr.txt");
+    let stdout = tmp.file("stdout.txt");
+
+    write_all(&stdin, "A");
+    run(&[
+        "--separator=@",
+        "-d=1",
+        "--@",
+        format!("--in=*e:{}", stdin).as_str(),
+        format!("--err={}", stderr).as_str(),
+        APP,
+        "pipe_loop",
+        "--@",
+        format!("--in=*e:{}", stdin).as_str(),
+        format!("--err={}", stderr).as_str(),
+        APP,
+        "pipe_loop",
+        "--@",
+        format!("--out={}", stdout).as_str(),
+        APP,
+        "try_open",
+        stdin.as_str(),
+    ])
+    .unwrap();
+
+    assert_eq!("AA", read_all(stderr));
     assert_eq!("err", read_all(stdout));
 }
 
