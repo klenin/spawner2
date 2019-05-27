bace7e5ee1065d994a618325ae3f91a2b80cc35f
ptslog <hsfk@yandex.ru>
2019-05-27 19:02:38 +1000

    Add tests on SP_* environment variables

--- spawner_tests/env.rs
+++ spawner_tests/env.rs
@@ -1,13 +1,24 @@
-use crate::common::{read_all, TmpDir, APP};
+use crate::common::{read_all, write_all, TmpDir, APP};
+use crate::term_reason::{
+    ensure_idle_time_limit_exceeded, ensure_mem_limit_exceeded, ensure_user_time_limit_exceeded,
+    ensure_wall_clock_time_limit_exceeded, ensure_write_limit_exceeded,
+};
 
-use spawner_driver::run;
+use spawner_driver::{run, Report};
+
+use std::env;
 
 struct Env {
     data: String,
 }
 
 impl Env {
-    fn new<T, U>(argv: T) -> Env
+    fn new() -> Env {
+        let empty_argv: [&'static str; 0] = [];
+        Env::with_argv(&empty_argv)
+    }
+
+    fn with_argv<T, U>(argv: T) -> Env
     where
         T: IntoIterator<Item = U>,
         U: AsRef<str>,
@@ -37,19 +48,19 @@ impl Env {
 
 #[test]
 fn clear_env() {
-    let env = Env::new(&["-env=clear"]);
+    let env = Env::with_argv(&["-env=clear"]);
     assert_eq!(env.vars(), Vec::new());
 }
 
 #[test]
 fn define_var() {
-    let env = Env::new(&["-env=clear", "-D:NAME=VAR"]);
+    let env = Env::with_argv(&["-env=clear", "-D:NAME=VAR"]);
     assert_eq!(env.vars(), vec![("NAME", "VAR")]);
 }
 
 #[test]
 fn define_var_2() {
-    let env = Env::new(&["-env=clear", "-D:A=B", "-D:C=D"]);
+    let env = Env::with_argv(&["-env=clear", "-D:A=B", "-D:C=D"]);
     let mut vars = env.vars();
     vars.sort_by(|a, b| a.0.partial_cmp(b.0).unwrap());
     assert_eq!(vars, vec![("A", "B"), ("C", "D")]);
@@ -57,13 +68,86 @@ fn define_var_2() {
 
 #[test]
 fn overwrite_var() {
-    let env = Env::new(&["-env=clear", "-D:NAME=VAR", "-D:NAME=VAR1"]);
+    let env = Env::with_argv(&["-env=clear", "-D:NAME=VAR", "-D:NAME=VAR1"]);
     assert_eq!(env.vars(), vec![("NAME", "VAR1")]);
 }
 
 #[test]
 fn default_env() {
-    let cmds: [&'static str; 0] = [];
-    let env = Env::new(&cmds);
+    let env = Env::new();
     assert!(env.vars().len() != 0);
+}
+
+fn run_with_env(key: &str, val: &str, argv: &[&str]) -> Vec<Report> {
+    env::set_var(key, val);
+    let r = run(argv).unwrap();
+    env::remove_var(key);
+    r
+}
+
+#[test]
+fn sp_time_limit() {
+    let r = run_with_env("SP_TIME_LIMIT", "0.5", &[APP, "loop", "1"]);
+    ensure_user_time_limit_exceeded(&r[0]);
+}
+
+#[test]
+fn sp_deadline() {
+    let r = run_with_env("SP_DEADLINE", "0.5", &[APP, "sleep", "1"]);
+    ensure_wall_clock_time_limit_exceeded(&r[0]);
+}
+
+#[test]
+fn sp_idle_time_limit() {
+    let r = run_with_env("SP_IDLE_TIME_LIMIT", "0.5", &[APP, "sleep", "1"]);
+    ensure_idle_time_limit_exceeded(&r[0]);
+}
+
+#[test]
+fn sp_memory_limit() {
+    let r = run_with_env("SP_MEMORY_LIMIT", "5", &[APP, "alloc", "5", "sleep", "1"]);
+    ensure_mem_limit_exceeded(&r[0]);
+}
+
+#[test]
+fn sp_write_limit() {
+    let r = run_with_env("SP_WRITE_LIMIT", "0", &[APP, "print_n", "AAAAA", "10000"]);
+    ensure_write_limit_exceeded(&r[0]);
+}
+
+#[test]
+fn sp_report_file() {
+    let tmp = TmpDir::new();
+    let report = tmp.file("report.txt");
+    run_with_env("SP_REPORT_FILE", &report, &[APP]);
+    assert!(read_all(report).len() != 0);
+}
+
+#[test]
+fn sp_environment() {
+    env::set_var("SP_ENVIRONMENT", "clear");
+    let env = Env::new();
+    env::remove_var("SP_ENVIRONMENT");
+    assert_eq!(env.vars(), Vec::new());
+}
+
+#[test]
+fn sp_input_output_error_file() {
+    let tmp = TmpDir::new();
+    let input = tmp.file("input.txt");
+    let output = tmp.file("output.txt");
+    let error = tmp.file("error.txt");
+
+    write_all(&input, "AAA");
+    env::set_var("SP_INPUT_FILE", &input);
+    env::set_var("SP_OUTPUT_FILE", &output);
+    env::set_var("SP_ERROR_FILE", &error);
+
+    let _ = run(&[APP, "pipe_loop"]);
+    env::remove_var("SP_INPUT_FILE");
+    env::remove_var("SP_OUTPUT_FILE");
+    env::remove_var("SP_ERROR_FILE");
+
+    assert_eq!(read_all(output), "AAA");
+    assert_eq!(read_all(error), "AAA");
 }
